df940069d36a335a9c27cf5f32cdb9cb69096182
SuebSaifi <sueb18-cs@danskar.org>
2022-06-23 18:55:53 +0530

    gen model controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'user/index'
   resources :faceposts
   root "faceposts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
