5dfd29fa533c5e57904ec55a149c48607b07233b
SuebSaifi <sueb18-cs@danskar.org>
2022-06-29 10:34:55 +0530

    database

--- db/migrate/20220624105727_remove_friend_id_from_friends.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFriendIdFromFriends < ActiveRecord::Migration[6.1]
-  def change
-  remove_column :friends, :freind_id,:integer
-  remove_column :friends, :friend_id
-  end
-end
--- db/migrate/20220624130751_add_friend_id_to_friends.rb
+++
@@ -1,5 +0,0 @@
-class AddFriendIdToFriends < ActiveRecord::Migration[6.1]
-  def change
-  add_column :friends, :friend_id, :integer
-  end
-end
